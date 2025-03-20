bde207b6d100e265bce174a6db3e0975eeb4c8cd
Jan Krukowski <jan.krukowski@geneva.com>
2025-03-20 18:44:56 +0100

    1st release

--- Package.swift
+++ Package.swift
@@ -5,10 +5,19 @@ import PackageDescription
 let package = Package(
     name: "llama.cpp.xcframework",
     products: [
+        .library(
+            name: "llama",
+            targets: ["llama"]
+        ),
     ],
     targets: [
         .target(
-            name: "_llama.cpp"
+            name: "llama.cpp"
         ),
+        .binaryTarget(
+            name: "llama",
+            url: "https://github.com/jkrukowski/llama.cpp.xcframework/releases/download/0.0.1/llama.cpp.xcframework.zip",
+            checksum: "55fa1c3675bdb5ba8de6ceb3cb029fc02a56c0f8379dd6533b53aba3df41d105"
+        )
     ]
 )
